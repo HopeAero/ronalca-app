3daf806d8b429076f7c48c74cf997eb6389c9b51
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-09-22 23:26:43 -0400

    feat: ajustar estilos del componente Banner; modificar dimensiones y mejorar la responsividad

--- src/components/categorias/banner.tsx
+++ src/components/categorias/banner.tsx
@@ -11,7 +11,7 @@ interface BannerProps {
 
 export default function Banner({ subcategories }: BannerProps) {
   return (
-    <div className="relative w-full h-46 md:h-64 overflow-hidden">
+    <div className="relative w-full h-40 md:h-64 overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
@@ -25,12 +25,12 @@ export default function Banner({ subcategories }: BannerProps) {
 
       {/* Content */}
       <div className="relative z-10 h-full flex flex-col justify-center px-4 md:px-8">
-        <h1 className="text-white text-4xl md:text-6xl font-bold leading-none tracking-wide">
+        <h1 className="text-white text-xl md:text-6xl font-bold leading-tight tracking-wide">
           AIRE ACONDICIONADO
         </h1>
         {subcategories ? (
           <h2
-            className={`text-2xl md:text-5xl font-black leading-none mt-4`}
+            className="text-xl md:text-5xl font-black leading-tight mt-2 md:mt-4"
             style={{
               fontFamily: "sans-serif",
               WebkitTextStroke: "1px white",
@@ -41,21 +41,28 @@ export default function Banner({ subcategories }: BannerProps) {
             {subcategories.name}
           </h2>
         ) : (
-          <Skeleton variant="text" width={300} height={50} />
+          <Skeleton variant="text" width={200} height={40} />
         )}
       </div>
 
       {/* Carrier Logo */}
-      <div className="absolute top-1/2 right-4 transform -translate-y-1/2 z-20">
+      <div className="absolute top-1/2 right-2 md:right-4 transform -translate-y-1/2 z-20">
         {subcategories ? (
-          <Image
-            src={subcategories.imageUrl || ""}
-            alt="Carrier logo"
-            width={250}
-            height={250}
-          />
+          <div className="w-24 h-24 md:w-64 md:h-64">
+            <Image
+              src={subcategories.imageUrl || ""}
+              alt="Carrier logo"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         ) : (
-          <Skeleton variant="rectangular" width={250} height={250} />
+          <Skeleton
+            variant="rectangular"
+            width={150}
+            height={150}
+            className="md:w-250 md:h-250"
+          />
         )}
       </div>
     </div>
