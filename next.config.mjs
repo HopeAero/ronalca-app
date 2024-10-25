c32e7f84493ce3861cc8478327ac418e4ab3e333
Emmanuel Salcedo <73862929+HopeAero@users.noreply.github.com>
2024-10-24 21:59:03 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_IMAGES_DOMAIN],
+    domains: [process.env.NEXT_PUBLIC_API_URL],
   },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
